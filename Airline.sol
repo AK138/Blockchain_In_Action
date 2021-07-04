75b20b262aabacf3107a8079e57064641fbf338f
AK138 <42497604+AK138@users.noreply.github.com>
2021-07-04 18:46:14 +0900

    Update Airline.sol

--- Airline.sol
+++ Airline.sol
@@ -1,21 +1,26 @@
 pragma solidity ^0.6.0;
 contract Airlines {
-    address chairperson;
-    struct details {
+    address chairperson; // address to refer to the identity of the chairperson
+    
+    // Airline data strucuture
+    struct details { // struct to collectively define the data of the airlines, including the escrow or the deposit
         uint escrow; // deposit for payment settlement
         uint status;
         uint hashOfDetails; 
         }
         
+        // Airline account payments and membership mapping
+        // mapping to map account addresses (identities) of members to their details (A mapping is like a hash table.)
         mapping (address=>details) public balanceDetails;
         mapping (address=>uint) membership;
         
         // modifiers or rules
+        // modifier for onlyChairperson rule
         modifier onlyChairperson{
             require(msg.sender==chairperson);
             _;
         }
-        
+        // modifier for onlyMember rule
         modifier onlyMember{
             require(membership[msg.sender]==1);
             _;
@@ -23,12 +28,15 @@ contract Airlines {
         }
         // constructor function
         constructor () public payable {
+            // use of msg.sender and msg.value for a payable function
             chairperson=msg.sender;
             membership[msg.sender]=1; // automatically registered
             balanceDetails[msg.sender].escrow = msg.value;
         }
         
-        function register ( ) public payable{
+        // function of the contract
+        function register ( ) public payable {
+            // use of Usage of msg.sender and msg.value for a payable function
             address AirlineA = msg.sender;
             membership[AirlineA]=1;
             balanceDetails[msg.sender].escrow = msg.value;
@@ -43,8 +51,7 @@ contract Airlines {
                 balanceDetails[AirlineZ].escrow = 0;
         }
         
-        function request(address toAirline, uint hashOfDetails) 
-        onlyMember public {
+        function request(address toAirline, uint hashOfDetails) onlyMember public {
             if(membership[toAirline]!=1){
                 revert();
             }
@@ -52,8 +59,7 @@ contract Airlines {
             balanceDetails[msg.sender].hashOfDetails = hashOfDetails;
         }
         
-        function response(address fromAirline, uint hashOfDetails, uint done)
-        onlyMember public {
+        function response(address fromAirline, uint hashOfDetails, uint done) onlyMember public {
             if(membership[fromAirline]!=1){
                 revert(); }
                 balanceDetails[msg.sender].status=done;
@@ -61,15 +67,16 @@ contract Airlines {
         }
         
         
-        function settlePayment (address payable toAirline) 
-        onlyMember payable public {
+        function settlePayment (address payable toAirline) onlyMember payable public {
             address fromAirline=msg.sender;
             uint amt = msg.value;
             balanceDetails[toAirline].escrow = balanceDetails[toAirline].escrow + amt;
             balanceDetails[fromAirline].escrow = balanceDetails[fromAirline].escrow - amt;
             
             // amt subtracted from msg.sender and given to toAirline
-            
+            // smart contract account transferring amount to an external account
             toAirline.transfer(amt);
         }
 }
+
+Ramamurthy, B. (2020). Blockchain in action. Manning Publications.
